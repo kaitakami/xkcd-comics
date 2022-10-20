4778df35f4c66c31a1bae15ec05bdaee21bb248e
Kai Takami <kaitakami@protonmail.com>
2022-10-20 21:42:47 +0900

    Code Header component for index page

---
+++ components/Header.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Link from "next/link";
+import { Container } from "@nextui-org/react";
+
+const Header = () => {
+  return (
+    <header className="flex justify-between items-center p-4 max-w-xl m-auto">
+      <h1 className="font-bold text-xl">
+        Next<span className="font-light">xkcd</span>
+      </h1>
+      <nav className="">
+        <ul className="flex flex-row gap-2 font-bold items-center">
+          <li className="mb-0">
+            <Link href="/">
+              <a className="text-xm font-semibold">Home</a>
+            </Link>
+          </li>
+          <li className="mb-0">
+            <Link href="/about">
+              <a className="text-xm font-semibold">About</a>
+            </Link>
+          </li>
+          <li className="mb-0">
+            <Link href="/search">
+              <a className="text-xm font-semibold">Search</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
